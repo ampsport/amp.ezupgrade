010671b0329b1be2db98b3e31889b4a57d6e19df
Elizabeth Leddy <eleddy@eastofeaton.com>
2012-05-11 10:37:33 -0700

    rename the forms and comment out stuff to get going faster

--- amp/ezupgrade/browser/qi.py
+++ amp/ezupgrade/browser/qi.py
@@ -1,5 +1,4 @@
-from Products.CMFPlone import PloneMessageFactory as _
-from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
 import logging
 from Products.Five.browser import BrowserView
 
@@ -10,6 +9,20 @@ class ManageProductsView(BrowserView):
     permissions issues
     """
 
+    def __init__(self, *args, **kwargs):
+        super(ManageProductsView, self).__init__(*args, **kwargs)
+        self.qi = getToolByName(self.context, 'portal_quickinstaller')
+
     def __call__(self):
         return self.index()
 
+    def installable_products(self):
+        return self.qi.listInstallableProducts()
+
+    def installed_products(self):
+        products = self.qi.listInstalledProducts()
+        valid = []
+        for product in products:
+            if not product['isHidden'] and not product['isLocked']:
+                valid.append(product)
+        return valid
